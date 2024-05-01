e6a0a4d5073db3669fd251b4f7263bf44f42125f
JoeD85 <joe_doran01@hotmail.com>
2024-05-01 15:18:09 +0000

    Fib series with loops

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include "mbed.h"
+#include <cstudio>
 
-// A Second Commit
+int t1 = 0;
+int t2 = 1;
+int next_term = t1 + t2;
+int max_terms = 8;
 
-// main() runs in its own thread in the OS
 int main()
 {
+    printf("Hello, Lab Program 1 \r\n");
+    printf("Fibonacci Sequence: %d , %d , " , t1 , t2);
+    for(int i = 3; i<=max_terms; i++){
+        printf("%d , " , next_term);
+        t1 = t2;
+        t2 = next_term;
+        next_term = t1 + t2;
+    }
+    printf("\r\n");
     while (true) {
-
     }
 }
 
